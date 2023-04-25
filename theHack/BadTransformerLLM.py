29c03966ce7ca0764f468d69634302263a1523f4
UEFI-code <zhuoshizhang@hotmail.com>
2023-04-25 22:05:54 +0800

    Continue Hack

--- theHack/BadTransformerLLM.py
+++ theHack/BadTransformerLLM.py
@@ -3,20 +3,19 @@ import torch.nn as nn
 class myBadTransfomer(nn.Module):
     def __init__(self):
         super().__init__()
-        self.li1 = nn.Linear(4096, 4096, bias=False)
-        self.li2 = nn.Linear(4096, 4096, bias=False)
-        #self.li3 = nn.Linear(4096, 4096, bias=False)
-        self.li4 = nn.Linear(4096, 4096)
-        self.out = nn.Linear(4096, 4096)
+        #self.li1 = nn.Linear(4096, 128, bias=False)
+        #self.li2 = nn.Linear(4096, 128, bias=False)
+        #self.li3 = nn.Linear(4096, 128, bias=False)
+        self.li4 = nn.Linear(4096, 4096, bias=False)
+        self.out = nn.Linear(4096, 4096, bias=False)
 
     def forward(self, x):
-        xA, xB = self.li1(x), self.li2(x)
-        xSqure = torch.matmul(xA.transpose(1, 2), xB)
+        #xA, xB, xC = self.li1(x), self.li1(x), self.li1(x)
+        xSqure = torch.matmul(x.transpose(1, 2), x)
         xSqure = torch.nn.functional.softmax(xSqure, dim=-1)
         xO = torch.matmul(x, xSqure)
-        #xO = xO.transpose(1, 2)
+        xO = torch.nn.functional.softmax(xO, dim=-1)
         xO = self.li4(xO)
         xO = torch.nn.functional.silu(xO)
-        x = self.out(x + xO)
-        return x
-    
+        xO = self.out(x + xO)
+        return xO
